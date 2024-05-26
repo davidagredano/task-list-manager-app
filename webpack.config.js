3541c41564bb60505386a4654f4e308c31647a38
David Agredano <davidagredano@gmail.com>
2024-05-26 17:37:53 +0200

    Add and configure webpack-dev-server dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
