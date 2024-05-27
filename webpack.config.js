8474718b9dae06f2bb91b5dbb05e76d00744627e
David Agredano <davidagredano@gmail.com>
2024-05-27 19:42:58 +0200

    Rename entry point to index.js instead of main.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/main.js",
+  entry: "./src/index.js",
   devServer: {
     static: "./dist",
   },
