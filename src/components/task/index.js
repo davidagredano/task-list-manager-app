1ea504a4f8982ddbbc806faf46ea0ddd10a36e42
David Agredano <davidagredano@gmail.com>
2024-06-05 13:08:04 +0200

    Refactor Task component for better structure
    This commit refactors the Task component. Previously, the Task component
    was only displaying the task title.
    
    This change introduces a more structured layout for the Task component.
    It now includes a Body function that appends a Checkbox and Content to
    the task. The Content function further appends a Title and Description
    to the task.
    
    This new structure allows for a more detailed and organized display of
    tasks, improving the user interface and experience.

--- src/components/task/index.js
+++ src/components/task/index.js
@@ -1,7 +1,54 @@
+import "./task.css";
+
+const Description = (description) => {
+  const p = document.createElement("p");
+  p.className = "task__description";
+  p.textContent = description;
+
+  return p;
+};
+
+const Title = (title) => {
+  const h3 = document.createElement("h3");
+  h3.className = "task__title";
+  h3.textContent = title;
+
+  return h3;
+};
+
+const Content = (task) => {
+  const div = document.createElement("div");
+  div.className = "task__content";
+
+  div.appendChild(Title(task.title));
+  div.appendChild(Description(task.description));
+
+  return div;
+};
+
+const Checkbox = (isCompleted) => {
+  const button = document.createElement("button");
+  button.className = "task__checkbox";
+  button.textContent = isCompleted ? "X" : "";
+
+  return button;
+};
+
+const Body = (task) => {
+  const div = document.createElement("div");
+  div.className = "task__body";
+
+  div.appendChild(Checkbox(task.completed));
+  div.appendChild(Content(task));
+
+  return div;
+};
+
 const Task = (task) => {
   const article = document.createElement("article");
   article.className = "task";
-  article.textContent = task.title;
+
+  article.appendChild(Body(task));
 
   return article;
 };
