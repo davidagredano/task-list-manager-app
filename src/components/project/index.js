bd6a20bff961e1836ac6429c2c2dc355f806be98
David Agredano <davidagredano@gmail.com>
2024-06-05 15:53:18 +0200

    Introduce project deletion functionality
    This commit introduces the ability to delete a project from the project
    component. A new 'DeleteProjectBtn' function has been added which creates
    a button that, when clicked, deletes the project with the given id and
    refreshes the project list and project board components.
    
    In addition, an 'Actions' function has been created which appends the
    delete button to the project header. This provides users with an easy
    and intuitive way to manage their projects.

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,6 +1,13 @@
-import { Task } from "..";
+import LocalStorageDataAccessor from "../../data/local-storage";
+import ProjectController from "../../features/project-controller";
+import { refreshComponent } from "../utils";
+
+import { ProjectListItems, ProjectBoard, Task } from "..";
 
 import "./project.css";
+
+const dataAccessor = new LocalStorageDataAccessor();
+const projectController = new ProjectController(dataAccessor);
 
 const Title = (projectData) => {
   const title = document.createElement("h2");
@@ -10,11 +17,35 @@ const Title = (projectData) => {
   return title;
 };
 
+const DeleteProjectBtn = (id) => {
+  const button = document.createElement("button");
+  button.classList.add("project__delete-btn");
+  button.textContent = "Delete";
+
+  button.addEventListener("click", () => {
+    projectController.deleteProject(id);
+    refreshComponent("project-list-component", ProjectListItems());
+    refreshComponent("project-board-component", ProjectBoard());
+  });
+
+  return button;
+};
+
+const Actions = (projectData) => {
+  const div = document.createElement("div");
+  div.classList.add("project__actions");
+
+  div.appendChild(DeleteProjectBtn(projectData.id));
+
+  return div;
+};
+
 const Header = (projectData) => {
   const header = document.createElement("header");
   header.classList.add("project__header");
 
   header.appendChild(Title(projectData));
+  header.appendChild(Actions(projectData));
 
   return header;
 };
